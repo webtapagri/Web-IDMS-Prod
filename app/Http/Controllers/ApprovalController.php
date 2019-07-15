b22d954b1909182497cf67cc00733470280db9f0
vanrayen <irvan27@gmail.com>
2019-07-15 14:04:15 +0700

    add create kode asset ams for amp

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -1662,7 +1662,7 @@ class ApprovalController extends Controller
         {   
             $sql = " CALL create_kode_asset_ams('".$noreg."', '".$ANLA_BUKRS."', '".$dt->JENIS_ASSET."', '-".$dt->ID."') ";
             DB::SELECT($sql);
-            echo "1<pre>"; print_r($sql); die();
+            //echo "1<pre>"; print_r($sql); die();
 
             DB::commit();
 
