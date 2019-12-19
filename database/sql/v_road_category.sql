2731718372ea4ab3e324c4e10532c27792637078
andipahlevy <andilevi@gmail.com>
2019-12-19 10:02:36 +0700

    change db objects to uppercase

--- database/sql/v_road_category.sql
+++ database/sql/v_road_category.sql
@@ -1,5 +1,5 @@
 create or replace view V_ROAD_CATEGORY as
 select trc.id, trs.status_name, trc.category_name, trc.category_code, trc.category_initial , trc.status_id
-from tm_road_category trc
-join tm_road_status trs on trs.id = trc.status_id
+from TM_ROAD_CATEGORY trc
+join TM_ROAD_STATUS trs on trs.id = trc.status_id
 where trc.deleted_at is null
