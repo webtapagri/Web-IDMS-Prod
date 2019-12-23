1d7fa24ad8eb5a40750068b20bd0b0607b059805
andipahlevy <andilevi@gmail.com>
2019-12-23 11:10:39 +0700

    change sidebar width

--- resources/views/layouts/global_style.blade.php
+++ resources/views/layouts/global_style.blade.php
@@ -3,4 +3,17 @@
     background: url({{ asset('limitless/global_assets/images/backgrounds/user_bg1.png') }}) center center no-repeat;
     background-size: cover;
 }
+
+.sidebar {
+    width: 14.875rem;]
+}
+
+.nav-group-sub .nav-link {
+    padding: .625rem 1.25rem .625rem 2.5rem;
+}
+
+.navbar-expand-md .navbar-brand {
+    min-width: 13.625rem;
+}
+
 </style>
